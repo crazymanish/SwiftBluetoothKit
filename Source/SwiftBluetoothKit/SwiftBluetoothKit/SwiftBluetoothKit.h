e52779a0635ca5e0592d7a9e4035c8f6f47c0e6e
Manish Rathi <manishrathi19902013@gmail.com>
2019-02-09 18:23:12 +0530

    Initial Commit of SwiftBluetoothKit

---
+++ Source/SwiftBluetoothKit/SwiftBluetoothKit/SwiftBluetoothKit.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBluetoothKit.h
+//  SwiftBluetoothKit
+//
+//  Created by Manish Rathi on 09/02/19.
+//  Copyright © 2019 Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftBluetoothKit.
+FOUNDATION_EXPORT double SwiftBluetoothKitVersionNumber;
+
+//! Project version string for SwiftBluetoothKit.
+FOUNDATION_EXPORT const unsigned char SwiftBluetoothKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBluetoothKit/PublicHeader.h>
+
+
