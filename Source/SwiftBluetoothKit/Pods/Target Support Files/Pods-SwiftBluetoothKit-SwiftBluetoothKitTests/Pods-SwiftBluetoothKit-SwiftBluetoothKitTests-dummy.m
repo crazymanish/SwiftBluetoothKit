72c8f0b41eaa6dac154683cd73bc11a7f0fa98d1
Manish Rathi <manishrathi19902013@gmail.com>
2019-02-09 18:34:36 +0530

    Added RxBluetoothKit Pod dependency.

---
+++ Source/SwiftBluetoothKit/Pods/Target Support Files/Pods-SwiftBluetoothKit-SwiftBluetoothKitTests/Pods-SwiftBluetoothKit-SwiftBluetoothKitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftBluetoothKit_SwiftBluetoothKitTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftBluetoothKit_SwiftBluetoothKitTests
+@end
